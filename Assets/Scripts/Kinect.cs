9893541eb99258e416385e06210bdf8eb29b4387
Bruno FACHE <b.fache@ludus-academie.com>
2019-11-29 10:59:30 +0100

    Mouvement des mains

---
+++ Assets/Scripts/Kinect.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Windows.Kinect;
+
+using System.Linq;
+
+public class Kinect : MonoBehaviour {
+
+    private KinectSensor _sensor;
+    private BodyFrameReader _bodyFrameReader;
+    private Body[] _bodies = null;
+
+    public static Kinect instance = null;
+
+    public Body[] GetData()
+    {
+        return _bodies;
+    }
+
+    // Use this for initialization
+    void Start () {
+        _sensor = KinectSensor.GetDefault();
+
+        if (_sensor != null)
+        {
+            _bodyFrameReader = _sensor.BodyFrameSource.OpenReader();
+
+            if (!_sensor.IsOpen)
+            {
+                _sensor.Open();
+            }
+
+            _bodies = new Body[_sensor.BodyFrameSource.BodyCount];
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (_bodyFrameReader != null)
+        {
+            var frame = _bodyFrameReader.AcquireLatestFrame();
+
+            if (frame != null)
+            {
+                frame.GetAndRefreshBodyData(_bodies);
+
+                if (_bodies == null)
+                {
+                    _bodies = new Body[_sensor.BodyFrameSource.BodyCount];
+                }
+
+                frame.GetAndRefreshBodyData(_bodies);
+
+                frame.Dispose();
+                frame = null;
+            }
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Mouvement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+
+public class Mouvement : MonoBehaviour {
+
+    public GameObject BodySrcManager;
+    public JointType TrackedJoint;
+    private Kinect bodyManager;
+    private Body[] bodies;
+    public float multiplier = 1;
+
+    // Use this for initialization
+    void Start () {
+        bodyManager = BodySrcManager.GetComponent<Kinect>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        bodies = bodyManager.GetData();
+
+        foreach (var body in bodies)
+        {
+            if (body == null)
+            {
+                continue;
+            }
+            if (body.IsTracked)
+            {
+                var pos = body.Joints[TrackedJoint].Position;
+                gameObject.transform.position = new Vector3(pos.X * multiplier, pos.Y * multiplier);
+
+            }
+        }
+
+    }
+}
